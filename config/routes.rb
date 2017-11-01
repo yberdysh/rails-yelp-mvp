c128be1fe6f407122f24de1c0c4faf137ab032c7
Jane Costa <yberdysh@gmail.com>
2017-11-01 12:18:52 -0400

    created controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:create, :index, :new, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
