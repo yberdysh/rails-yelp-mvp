469087571f4b7e5ce1353acdf5a2993c2f5ba1a7
Jane Costa <yberdysh@gmail.com>
2017-11-01 11:18:40 -0400

    finished restaurant model and created controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  # has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in:
+    %w(italian japanese french belgian chinese russian mexican ethiopian thai indonesian vietnamese),
+    allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,10 @@ ActiveRecord::Schema.define(version: 20171101144732) do
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.string   "phone"
+    t.string   "phone_number"
     t.string   "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
