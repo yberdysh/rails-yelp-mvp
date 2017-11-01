20702e39d0c36a023baa982f5ee2471d8b99170d
Jane Costa <yberdysh@gmail.com>
2017-11-01 10:59:47 -0400

    added faker gem and seeded database with restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# A restaurant must have at least a name and an address.
+# The restaurant category should belong to a fixed list
+# ["chinese", "italian", "japanese", "french", "belgian"].
+# A review must have a parent restaurant.
+# A review must have content and a rating.
+# The rating should be a number between 0 and 5.
+# When a restaurant is destroyed, all of its reviews should be destroyed as well.
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+# Review.destroy_all
+
+puts 'Creating restaurants and reviews...'
+
+20.times do
+  Restaurant.create!(
+    name: [
+      Faker::Food.spice,
+      Faker::Food.ingredient,
+      Faker::Lovecraft.deity,
+      Faker::Lovecraft.location
+      ].sample,
+    address: Faker::Address.street_address,
+    phone: Faker::PhoneNumber.cell_phone,
+    category: %w(italian japanese french belgian chinese russian mexican ethiopian thai indonesian vietnamese).sample
+    )
+end
+
+puts 'Finished!'
+
+    # 10.times do
+    #   Article.create(title: Faker::HitchhikersGuideToTheGalaxy.quote, content: Faker::Lorem.paragraphs.join)
+    # end
